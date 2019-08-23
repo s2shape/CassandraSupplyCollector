e3054962c107a44119c4aefd0a4fff923fb7f207
MartinStival <54437721+MartinStival@users.noreply.github.com>
2019-08-23 10:25:57 -0700

    update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#docker run --name some-cassandra -d -e CASSANDRA_BROADCAST_ADDRESS=192.168.1.82 -p 7000:7000 cassandra:3
-
-#sleep 10
-#sudo docker cp PostgresSupplyCollectorTests/tests/data.sql postgres:/docker-entrypoint-initdb.d/data.sql
-#sudo docker exec -u postgres postgres psql postgres postgres -f docker-entrypoint-initdb.d/data.sql
+dotnet build
+sudo docker rm -f -v cassandra
+sudo docker run --name cassandra -v /tmp:/var/lib/cassandra -d -e CASSANDRA_BROADCAST_ADDRESS=127.0.0.1 -p 9042:9042 cassandra:3
+sleep 10
+sudo cp CassandraSupplyCollectorTests/tests/data.sql /tmp
+sudo docker exec -i cassandra cqlsh -f /var/lib/cassandra/data.sql
 dotnet test
-#sudo docker stop postgres
-#sudo docker rm postgres
+sudo docker stop cassandra
+sudo docker rm cassandra
+sudo rm /tmp/data.sql
